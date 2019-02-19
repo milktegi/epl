7cb2ef93d959b9ba0cdde31937bb5d77f3967129
milktegi <milkteegi@gmail.com>
2019-02-19 12:22:38 +0900

    implemented layout

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 
 import { Route, Switch } from 'react-router-dom';
 
-import Home from './components/Home/home'
+import Home from './components/Home/home';
+import Layout from './hoc/Layout/layout';
 
 class Routes extends Component {
   render() {
     return (
-      <div>
+      <Layout>
         <Switch>
           <Route path='/' exact component={Home} />
         </Switch>
-      </div>
+      </Layout>
     );
   }
 }
