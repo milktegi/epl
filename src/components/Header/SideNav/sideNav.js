ce32be519bc6e4a401e14852b2fb1db2945506da
milktegi <milkteegi@gmail.com>
2019-02-19 12:51:33 +0900

    added toggle function

---
+++ src/components/Header/SideNav/sideNav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import SideNav from 'react-simple-sidenav';
+
+const SideNavigation = (props) => {
+	return(
+		<div>
+			<SideNav
+				showNav={props.showNav}
+				onHideNav={props.onHideNav}
+				navStyle={{
+					background: '#242424',
+					maxWidth: '220px'
+				}}
+			>
+				
+				
+				<div>
+					home
+				</div>
+				</SideNav>
+		</div>
+	)
+}
+
+export default SideNavigation;
