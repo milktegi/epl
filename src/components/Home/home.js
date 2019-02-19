e8fa972fa5863d12a50609dd8e11f7c6a1490626
milktegi <milkteegi@gmail.com>
2019-02-19 14:15:50 +0900

    added slider

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import NewsSlider from '../widgets/NewsSlider/sldier';
 
 const Home = () => {
 	return(
 		<div>
-			home
+		 	<NewsSlider/>
 		</div>
 	)
 }
---
+++ src/components/widgets/NewsSlider/sldier.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { URL } from '../../../config';
+
+import SliderTemplates from './slider_templates';
+
+class NewsSlider extends Component {
+  state = {
+    news: []
+  };
+
+  componentDidMount() {
+		axios
+		.get(`${URL}/article?_start=0&_end=3`)
+		.then((response) => {
+			// console.log(response);
+			this.setState({
+				news: response.data
+			})
+    });
+  }
+
+  render() {
+		console.log(this.state.news)
+    return( 
+				<SliderTemplates
+				data={this.state.news}
+				
+				/>
+		);
+  }
+}
+
+export default NewsSlider;
