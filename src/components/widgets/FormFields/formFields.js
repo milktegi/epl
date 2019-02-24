c4dc23c9e53cf71a20d3782bd40c29bcea5c5a33
milktegi <milkteegi@gmail.com>
2019-02-24 11:59:46 +0900

    adding validation

--- src/components/widgets/FormFields/formFields.js
+++ src/components/widgets/FormFields/formFields.js
@@ -1,36 +1,59 @@
-import React from 'react'
-import style from './formFields.css'
+import React from 'react';
+import styles from './formFields.css'
 
-const FormFields = ({ change, formdata, id }) => {
+
+
+const Formfields = ({formdata, change, id}) => {
 	
-	const renderTempalte = () => {
+	
+	const showError = () => {
+		let errorMessage = null;
+		
+		if(formdata.validation && !formdata.valid){
+				errorMessage = (
+					<div className={styles.labelError}>
+						{formdata.validationMessage}
+					</div>
+				)
+		}
+		
+		
+		return errorMessage;
+	}
+	
+	
+	const renderTemplate = () => {
 		let formTemplate = null;
-		switch(formdata.element) {
-			case('input'):
+	
+		switch(formdata.element){
+			case('input'):		
 			formTemplate = (
-				<div>
-					<Input 
-						{...formdata.config}
-						value={formdata.value}
-						onBlur={(event) => change({event, id, blur: true })}
-						onChange={(event) => change({event, id, blur: false })}
-						/>
-				</div>
-			)
-			break;
-			default:
+					<div>
+							<input
+								{...formdata.config}
+								value={formdata.value}
+								onBlur={(event)=> change({event, id, blur: true })}
+								onChange={(event)=> change({event, id, blur: false })}
+							/> 
+							{showError()}
+					</div>
+				)
+			
+						break;
+			default: 
 			formTemplate = null;
-		}	
+		}
 		return formTemplate;
 	}
-	 
+	
 	
 	return(
 		<div>
-			{renderTempalte()}
+			{renderTemplate()}
 		</div>
 	)
+	
+
 }
 
-
-export default FormFields; 
+export default Formfields;
